d70f72d9bbed4008e1490008733a48e1e4ee081f
Cameron Maclean <cameronmaclean17@gmail.com>
2020-02-08 22:52:18 -0800

    Graphics part 4

---
+++ Assets/Branched Programming Domains/Graphics/temple/Temple_Animations.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Temple_Animations : MonoBehaviour
+{
+    public float speedx;
+    public float speedy;
+    public float speedz;
+    public int directionx;
+    public int directiony;
+    public int directionz;
+    public float speedMult;
+    float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speedMult = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time = Time.deltaTime;
+        transform.Rotate(speedx * directionx * time * speedMult, speedy * directiony * time * speedMult, speedz * directionz * time* speedMult);
+    }
+}
