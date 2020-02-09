036da5de6b62c2a9799f916e5c87e90cbe55387a
Cameron Maclean <cameronmaclean17@gmail.com>
2020-02-08 22:57:19 -0800

    Graphics part 5

---
+++ Assets/Branched Programming Domains/Graphics/Daniel Meyer/Level Design/ExtraGraphics/Building_strucs/Orbiting.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbiting : MonoBehaviour
+{
+    public GameObject sphere;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OrbitAround();
+    }
+
+    void OrbitAround()
+    {
+        transform.RotateAround(sphere.transform.position, Vector3.forward, speed * Time.deltaTime);
+    }
+}
