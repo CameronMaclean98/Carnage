235b40a48db637134e8d13eea587370309ccdfbc
Cameron Maclean <cameronmaclean17@gmail.com>
2020-02-20 17:13:34 -0800

    Units 1

---
+++ Assets/Branched Programming Domains/Graphics/Daniel Meyer/Daniel Assets/Prefabs/RTS_Buildings_Lit/Scripts/AddAltarSingleton.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+[RequiresEntityConversion]
+public class AddAltarSingleton : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public int teamnum;
+
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+
+
+
+        var hp = new Health
+        {
+            Max = 2000,
+            Value = 2000,
+            Type = ArmorType.Fortified
+        };
+        dstManager.AddComponentData(entity, hp);
+
+
+        var tag = new TagBuilding
+        {
+
+        };
+        dstManager.AddComponentData(entity, tag);
+
+        var tag2 = new TagAltar
+        {
+
+        };
+        dstManager.AddComponentData(entity, tag2);
+
+        var team = new Team
+        {
+            Number = teamnum,
+        };
+        dstManager.AddComponentData(entity, team);
+
+
+    }
+}
---
+++ Assets/Branched Programming Domains/Graphics/Daniel Meyer/Daniel Assets/Prefabs/RTS_Buildings_Lit/Scripts/AddBuildingSingletons.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+[RequiresEntityConversion]
+public class AddBuildingSingletons : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public int hpnum;
+    public int teamnum;
+
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+
+       
+
+        var hp = new Health
+        {
+            Max = hpnum,
+            Value = hpnum,
+            Type = ArmorType.Heavy
+        };
+        dstManager.AddComponentData(entity, hp);
+        
+
+        var tag = new TagBuilding
+        {
+
+        };
+        dstManager.AddComponentData(entity, tag);
+
+
+        var team = new Team
+        {
+            Number = teamnum,
+        };
+        dstManager.AddComponentData(entity, team);
+
+
+    }
+}
